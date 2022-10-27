cc315c9efaccc4b5e793d360d3c20c53d8381868
mgpai <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>
2022-10-27 13:09:24 +0530

    added script

---
+++ eventscripter/javascript/move-or-delete-extracted-files-from-sub-folders-based-on-filetypes.js
@@ -0,0 +1,21 @@
+/*
+    move or deleted extracted files from sub-folders based on filetypes
+    trigger: archive extraction finished
+*/
+
+var baseFolder = archive.extractToFolder || archive.folder;
+
+var exts = {
+    delete: ["inf"],
+    move: ["jpg", "jpeg"]
+}
+
+archive.extractedFilePaths.forEach(function(filePath) {
+    var ext = filePath.extension;
+
+    if (exts.delete.indexOf(ext) > -1) {
+        filePath.delete();
+    } else if (exts.move.indexOf(ext) > -1) {
+        filePath.parent != baseFolder && filePath.moveTo("c:/my folder");
+    }
+})
